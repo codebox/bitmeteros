81f57f0da38cfbd5ca0987bc3137f7dcf10d67a1
codebox_rob <codebox_rob>
2010-09-26 10:11:14 +0000

    *** empty log message ***

---
+++ webserver/web/m/js/bitmeter_mobile.js
@@ -0,0 +1,128 @@
+var formatAmount = (function(){
+	var K = 1024;
+	var KB_MIN = K;
+	var MB_MIN = KB_MIN * K;
+	var GB_MIN = MB_MIN * K;
+	var TB_MIN = GB_MIN * K;
+	var PB_MIN = TB_MIN * K;
+	var EB_MIN = PB_MIN * K;
+
+	return function (amt){
+		var numAmt, units;
+		if (amt < KB_MIN){
+			numAmt = amt.toFixed(2);
+			units = 'B';
+		} else if (amt < MB_MIN){
+			numAmt = (amt/KB_MIN).toFixed(2);
+			units = 'kB';
+		} else if (amt < GB_MIN){
+			numAmt = (amt/MB_MIN).toFixed(2);
+			units = 'MB';
+		} else if (amt < TB_MIN){
+			numAmt = (amt/GB_MIN).toFixed(2);
+			units = 'GB';
+		} else if (amt < PB_MIN){
+			numAmt = (amt/TB_MIN).toFixed(2);
+			units = 'TB';
+		} else {
+			numAmt = (amt/PB_MIN).toFixed(2);
+			units = 'PB';
+		}
+		return numAmt + ' ' + units;
+	};
+})();
+function showError(msg){
+	//TODO
+	alert(msg);
+}
+function buildAjaxRequest(){
+	var request = {}, onSuccess, onError;
+
+	function makeXHRObject(){
+		var xHRObject;
+		
+        if (window.XMLHttpRequest) {
+        	try {
+    			xHRObject = new XMLHttpRequest();
+            } catch(e) {
+    			xHRObject = null;
+            }
+            
+        } else if (window.ActiveXObject) {
+           	try {
+            	xHRObject = new ActiveXObject("Msxml2.XMLHTTP");
+          	} catch(e) {
+            	try {
+              		xHRObject = new ActiveXObject("Microsoft.XMLHTTP");
+            	} catch(e) {
+              		xHRObject = null;
+            	}
+    		}
+        }
+	        
+	    return xHRObject;
+	};	
+	
+	function buildResponseHandler( xHRObject ){
+	 // Form a closure containing the objects we need to deal with the response
+		return function(){
+			if ( xHRObject.readyState == 4 ) {
+	            if ( xHRObject.status >= 200 && xHRObject.status <= 299 ) {
+	            	if (onSuccess){
+		            	onSuccess( xHRObject.responseText );
+		            }
+	            
+	            } else {
+	             // There was an HTTP error
+	             	if (onError) {
+		                onError( 'HTTP ERROR: The response code was ' + xHRObject.status );
+		            }
+	            }
+	        } 
+		}; 	
+	}
+	
+	request.setSuccessHandler = function(handler){
+		onSuccess = handler;
+	};
+	request.setErrorHandler = function(handler){
+		onError = handler;
+	};
+	
+	request.send = function(url){
+		var xHRObject = makeXHRObject();
+		if (xHRObject === null){
+			showError();
+		} else {
+			xHRObject.onreadystatechange = buildResponseHandler( xHRObject );
+			
+			xHRObject.open('GET', url, true );
+			xHRObject.send(null);
+		}
+	};
+	
+	return request;
+};
+
+function monitorOnLoad(){
+	var REFRESH_SECS = 5;
+	var dlAmount = document.getElementById('dlAmount');
+	var ulAmount = document.getElementById('ulAmount');
+	
+	setInterval(function(){
+		var req = buildAjaxRequest();
+		req.setErrorHandler(showError);
+		req.setSuccessHandler(function(txt){
+			var obj = eval('(' + txt + ')');
+			var itemCount = obj.data.length;
+			var i, dlTotal = 0, ulTotal = 0;
+			for(i=0; i<itemCount; i++){
+				dlTotal += obj.data[i].dl;
+				ulTotal += obj.data[i].ul;
+			}
+			dlAmount.innerHTML = formatAmount(dlTotal/REFRESH_SECS);
+			ulAmount.innerHTML = formatAmount(ulTotal/REFRESH_SECS);
+		});
+		req.send('/m/monitor?ts=5');
+	}, REFRESH_SECS * 1000);
+}
