181a40d80bdfb9d7ebfac88ff54b36c2098a3e36
Rob Dawson <rob@codebox.org.uk>
2015-09-18 05:51:52 +0100

    Update runweb.sh
    fixing url used by runweb.sh

--- build/linux/debian/var/lib/bitmeter/runweb.sh
+++ build/linux/debian/var/lib/bitmeter/runweb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-WEB_URL=http://localhost:2605
+WEB_URL=http://localhost:2605/index.html
 BROWSER=$(which gnome-www-browser || which x-www-browser || which firefox || which www-browser)
 if [ "$BROWSER" = "" ]; then
 	echo For help visit $WEB_URL
